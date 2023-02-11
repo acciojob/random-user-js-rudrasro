f4281b10d86f463ec2f5f92e343b202bd260bc3e
Supriyo Rudra <117836404+rudrasro@users.noreply.github.com>
2023-02-11 21:33:16 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your code here
+const getUserButton = document.getElementById("getUser");
+const fullName = document.getElementById("fullName");
+const photo = document.getElementById("photo");
+const additionalInfo = document.getElementById("additionalInfo");
+
+getUserButton.addEventListener("click", function() {
+  fetch("https://randomuser.me/api/")
+    .then(response => response.json())
+    .then(data => {
+      const user = data.results[0];
+      fullName.innerText = user.name.first + " " + user.name.last;
+      photo.src = user.picture.large;
+      fullName.dataset.age = user.dob.age;
+      fullName.dataset.email = user.email;
+      fullName.dataset.phone = user.phone;
+      additionalInfo.innerText = "";
+    });
+});
+
+const ageButton = document.querySelector('[data-attr="age"]');
+const emailButton = document.querySelector('[data-attr="email"]');
+const phoneButton = document.querySelector('[data-attr="phone"]');
+
+ageButton.addEventListener("click", function() {
+  additionalInfo.innerText = "Age: " + fullName.dataset.age;
+});
+
+emailButton.addEventListener("click", function() {
+  additionalInfo.innerText = "Email: " + fullName.dataset.email;
+});
+
+phoneButton.addEventListener("click", function() {
+  additionalInfo.innerText = "Phone: " + fullName.dataset.phone;
+});
